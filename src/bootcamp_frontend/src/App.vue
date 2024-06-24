27a90ec4fba04f0a6784c6bd4b326d17f6cf62e8
Wiktor Wilk <70075488+WiktorDev@users.noreply.github.com>
2024-06-24 23:40:15 +0200

    first commit

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+let greeting = ref('');
+let counter = ref(0);
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const target = e.target;
+  const name = target.querySelector('#name').value;
+  greeting.value = `Hello ${name}`;
+}
+
+const decrease = () => {
+  counter.value--;
+};
+
+const increase = () => {
+  counter.value++;
+};
+</script>
+
+<template>
+  <main>
+    <form action="#" @submit="handleSubmit">
+      <label for="name">Enter your name: &nbsp;</label>
+      <input id="name" alt="Name" type="text" />
+      <button type="submit">Click Me!</button>
+    </form>
+    <section id="greeting">{{ greeting }}</section>
+
+    <section>
+      <button @click="decrease">-</button>
+      {{ counter }}
+      <button @click="increase">+</button>
+    </section>
+
+  </main>
+</template>
